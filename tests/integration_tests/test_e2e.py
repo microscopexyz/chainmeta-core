935e21345c311e505e537bf5562d099e35fb87c2
Zhanwu Xiong <488359+zhanwu@users.noreply.github.com>
2024-03-28 09:19:04 -0700

    write valid_from, valid_to and additional_metadata columns to db (#3)
    * write valid_from, valdi_to and additional_metadata columns to db

---
+++ tests/integration_tests/test_e2e.py
@@ -0,0 +1,33 @@
+# Copyright 2023 The chainmetareader Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pathlib
+
+
+def test_upload():
+    import chainmeta
+
+    chainmeta.set_connection_string(
+        "mysql+pymysql://root:test@127.0.0.1:3306/chainmeta"
+    )
+
+    data_folder = pathlib.Path(__file__).parent.resolve().joinpath("../data")
+    resolved_input_file = data_folder.joinpath(
+        "coinbase_additional_metadata_sample.json"
+    )
+    with open(resolved_input_file) as fp:
+        m = chainmeta.load(fp, artifact_base_path=data_folder)
+        total = chainmeta.upload_chainmeta(m["chainmetadata"]["artifact"])
+        if total == 0:
+            assert False, "make sure the local db is up and running"
+        assert total == 21
